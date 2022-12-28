567b50baa5a3a3e7595ebd3d38c0bc3c60b23042
= <=>
2022-12-28 18:23:05 +0800

    main

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Folder:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 85);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 18);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 122);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = folderBrowserDialog1;
+            folderBrowserDialog.ShowNewFolderButton = true;
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+                Environment.SpecialFolder specialFolder = folderBrowserDialog.RootFolder;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String inputdir = textBox1.Text;
+            string [] inputfiles = Directory.GetFiles(inputdir);
+            string [] csvset = new string[10];
+            string [] data1set = new string[10];
+            string [] data2set = new string[10];
+            string [] data3set = new string[10];
+            string [] data4set = new string[10];
+            String msg = "";
+            String data = "";
+            String data1 = "";
+            String data2 = "";
+            String data3 = "";
+            String data4 = "";
+            String text = "";
+            float data1sum = 0;
+            float data2sum = 0;
+            float data1avg, data2avg = 0;
+            double value = 0;
+            for(int j = 0; j < inputfiles.Length; j++)
+            {
+                msg = inputfiles[j] + "\r\n";
+                StreamReader inputfile = new StreamReader(inputfiles[j]);
+                int i = 0;
+                csvset = new string[10];
+                data1set = new string[10];
+                data2set = new string[10];
+                data3set = new string[10];
+                data4set = new string[10];
+                data1 = "";
+                data2 = "";
+                data3 = "";
+                data4 = "";
+                data1sum = 0;
+                data2sum = 0;
+                data1avg = 0;
+                data2avg = 0;
+                text = "";
+                while (!inputfile.EndOfStream)
+                {
+                    data = inputfile.ReadLine();
+                    if (data != null) csvset[i] = data;
+                    if (i == 9) i = 0;
+                    else i += 1;
+
+                }
+                for (i = 0; i < csvset.Length; i++)
+                {
+                    data1 = "";
+                    data2 = "";
+                    int count = 0;
+                    foreach (char w in csvset[i])
+                    {
+                        if (w != ',')
+                        {
+                            if (count == 3) data1 += w;
+                            if (count == 4) data2 += w;
+                            if (count == 5) data3 += w;
+                            if (count == 6) data4 += w;
+                        }
+                        if (w == ',') count += 1;
+                    }
+                    data1set[i] = data1;
+                    data2set[i] = data2;
+                    data3set[i] = data3;
+                    data4set[i] = data4;
+                }
+                for (i = 0; i < data1set.Length; i++)
+                {
+                    msg = msg + data1set[i] + "," + data2set[i] + "\r\n";
+                    data1sum += float.Parse(data1set[i]);
+                    data2sum += float.Parse(data2set[i]);
+                }
+                data1avg = data1sum / data1set.Length;
+                data2avg = data2sum / data2set.Length;
+                msg += "Avg:" + data1avg + "," + data2avg + "\r\n";
+                Random ran = new Random();
+                int r = ran.Next(0, 6);
+                switch (r)
+                {
+                    case 0:
+                        value = -0.003;
+                        break;
+                    case 1:
+                        value = -0.002;
+                        break;
+                    case 2:
+                        value = -0.001;
+                        break;
+                    case 3:
+                        value = 0;
+                        break;
+                    case 4:
+                        value = 0.001;
+                        break;
+                    case 5:
+                        value = 0.002;
+                        break;
+                    case 6:
+                        value = 0.003;
+                        break;
+                }
+                double newdata1 = Math.Round(data1avg + value, 3);
+                msg += "New Data: " + newdata1 + ",";
+                r = ran.Next(0, 6);
+                switch (r)
+                {
+                    case 0:
+                        value = -0.003;
+                        break;
+                    case 1:
+                        value = -0.002;
+                        break;
+                    case 2:
+                        value = -0.001;
+                        break;
+                    case 3:
+                        value = 0;
+                        break;
+                    case 4:
+                        value = 0.001;
+                        break;
+                    case 5:
+                        value = 0.002;
+                        break;
+                    case 6:
+                        value = 0.003;
+                        break;
+                }
+                double newdata2 = Math.Round(data2avg + value, 3);
+                msg += newdata2 + "\r\n";
+                text += "\r\n" + ",,," + newdata1 + "," + newdata2 + "," + data3set[0] + "," + data4set[0];
+                inputfile.Close();
+                FileInfo outputfileinfo = new FileInfo(inputfiles[j]);
+                StreamWriter outputfile = outputfileinfo.AppendText();
+                outputfile.WriteLine(text);
+                outputfile.Flush();
+                outputfile.Close();
+                //MessageBox.Show(msg);
+            }
+            MessageBox.Show("Finish!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
